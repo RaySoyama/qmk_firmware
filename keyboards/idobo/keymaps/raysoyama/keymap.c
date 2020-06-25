63769c6173f2f1694416e895a90e1163c8118cf8
Ray Soyama <raysoyama@gmail.com>
2020-06-25 10:05:54 -0700

    removed capslock from idobo75

--- keyboards/idobo/keymaps/raysoyama/keymap.c
+++ keyboards/idobo/keymaps/raysoyama/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_5x15(
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_PSCR,    KC_SLCK,    KC_PAUS,        KC_6,       KC_7,   KC_8,       KC_9,       KC_0,       KC_BSPC,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_INS,     KC_HOME,    KC_PGUP,        KC_Y,       KC_U,   KC_I,       KC_O,       KC_P,       KC_BSPC,
-    KC_CLCK,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_DEL,     KC_END,     KC_PGDN,        KC_H,       KC_J,   KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+    _______,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_DEL,     KC_END,     KC_PGDN,        KC_H,       KC_J,   KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       _______,    KC_UP,      _______,        KC_N,       KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,
     KC_LCTL,    KC_LGUI,    KC_LALT,    LOWER,      KC_SPC,     KC_SPC,     KC_LEFT,    KC_DOWN,    KC_RIGHT,       KC_SPC,     RAISE,  KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT),
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LEAGUE] = LAYOUT_ortho_5x15(
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_NO,    KC_NO,    KC_NO,        KC_6,       KC_7,   KC_8,       KC_9,       KC_0,       KC_BSPC,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,     KC_NO,    KC_NO,        KC_Y,       KC_U,   KC_I,       KC_O,       KC_P,       KC_BSPC,
-    KC_CLCK,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,     KC_NO,    KC_NO,        KC_H,       KC_J,   KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+    _______,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,     KC_NO,    KC_NO,        KC_H,       KC_J,   KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_NO,    KC_NO,    KC_NO,        KC_N,       KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,    KC_ENT,
     KC_LCTL,    KC_LGUI,    KC_LALT,    LOWER,      KC_SPC,     KC_SPC,     KC_NO,    KC_NO,    KC_NO,        KC_SPC,     RAISE,  _______,    _______,    _______,    _______),
 
